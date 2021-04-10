a9fc6be0d467212968ef7106a644517498d405e3
Moss Prescott <moss@theprescotts.com>
2021-04-10 16:57:23 -0400

    Simple structural equality

--- Sources/MammalKernel/Node.swift
+++ Sources/MammalKernel/Node.swift
@@ -66,7 +66,7 @@ public enum Value {
 /// Primitive values which may appear as attributes/elements. Note: more primitives will be added as
 /// necessary, but the intention is to cover only the most commonly-used values, plus a very general
 /// container for embedding arbitrary data.
-public enum Primitive {
+public enum Primitive: Equatable {
     case Nil
     case Bool(Bool)
     case Int(Int)
@@ -97,4 +97,85 @@ public enum NodeContent {
 /// - TODO: what else?
 public func checkWellFormedness(value: Value) -> Bool {
     fatalError("TODO")
+}
+
+
+// MARK: - Structural equality
+
+/// TODO: include NodeId or path or something as a location for each diff
+public enum ValueDelta: Equatable {
+    case primitivesDiffer(Primitive, Primitive)
+    case nodeTypesDiffer(NodeType, NodeType)
+    case attributeMissing(AttrName)
+    case attributeAdded(AttrName)
+    case elementMissing  // TODO: include what info?
+    case elementAdded  // TODO: include what info?
+    case contentTypesDiffer(String, String)  // TODO: what info, actually?
+}
+
+extension Value {
+    /// Compare two trees, ignoring ids but reporting all other differences.
+    /// The value on the right is considered the "new" value: if it contains somethng thats not
+    /// in the left value, the new thing is "added"; if the reverse, the thing is "missing"
+    public func diff(_ newValue: Value) -> [ValueDelta] {
+        switch (self, newValue) {
+        case (.Prim(let l), .Prim(let r)):
+            if l != r {
+                return [.primitivesDiffer(l, r)]
+            }
+            else {
+                return []
+            }
+        case (.Node(_, let lType, _), .Node(_, let rType, _))
+                where lType != rType:
+            return [.nodeTypesDiffer(lType, rType)]
+
+        case (.Node(_, _, let lContent), .Node(_, _, let rContent)):
+            switch (lContent, rContent) {
+
+            case (.Attrs(let lAttrs), .Attrs(let rAttrs)):
+                let lNames = Set(lAttrs.keys)
+                let rNames = Set(rAttrs.keys)
+                let missing = Array(lNames.subtracting(rNames))
+                    .map { ValueDelta.attributeMissing($0) }
+                let added = Array(rNames.subtracting(lNames))
+                    .map { ValueDelta.attributeAdded($0) }
+                let children = Array(lNames.union(rNames)).flatMap { name in
+                    lAttrs[name]!.diff(rAttrs[name]!)
+                }
+                return missing + added + children
+
+            case (.Elems(let lElems), .Elems(let rElems)):
+                // TODO: detect insertions and deletions, when some matching elements are present
+                let missing = Array(repeating: ValueDelta.elementMissing,
+                                    count: lElems.count - rElems.count)
+                let added = Array(repeating: ValueDelta.elementAdded,
+                                  count: rElems.count - lElems.count)
+                let children = zip(lElems, rElems).flatMap { (l, r) in
+                    l.diff(r)
+                }
+                return missing + added + children
+
+            case (.Ref(let lTarget), .Ref(let rTarget)):
+                fatalError("TODO")
+
+            case (.Empty, .Empty):
+                return []
+
+            case (_, _):
+                func contentType(_ content: NodeContent) -> String {
+                    switch content {
+                    case .Attrs(_): return "Attrs"
+                    case .Elems(_): return "Elems"
+                    case .Ref(_): return "Ref"
+                    case .Empty: return "Empty"
+                    }
+                }
+                return [.contentTypesDiffer(contentType(lContent), contentType(rContent))]
+            }
+
+        case (_, _):
+            fatalError("what am I missing?")
+        }
+    }
 }
