e31e80999b26aa5a8210202321b0cd5fc72a718f
Moss Prescott <moss@theprescotts.com>
2021-04-27 14:06:51 -0400

    More utilities for extracting attributes

--- Sources/MammalKernel/Util/Inject.swift
+++ Sources/MammalKernel/Util/Inject.swift
@@ -62,3 +62,64 @@ public func requiredAttr<T>(
         return .left("Missing required attribute \(attr) at node \(node.id) (no attributes present)")
     }
 }
+
+public func requiredAttrNode<T>(
+    _ node: Node, _ attr: AttrName,
+    expected: String,
+    handle: (Node) -> T?
+    ) -> Either<String, T> {
+    requiredAttr(node, attr, expected: expected) { val in
+        switch val {
+        case .Prim(_):
+            return nil
+        case .Node(let node):
+            return handle(node)
+        }
+    }
+}
+
+/// Extract an attribute and attempt to translate it to some value, or if the attribute is missing, substitute the provided default; returning
+/// either the result or a String describing what went wrong.
+/// Note: if the node isn't capable of having attributes (because it has `Elems` or a `Ref`), it's an error, but not if it's `Empty` (which
+/// is considered isomorphic to `Attrs` with none present.)
+public func optionalAttr<T>(
+    _ node: Node, _ attr: AttrName,
+    expected: String,
+    defaultValue: T,
+    handle: (Node.Value) -> T?
+) -> Either<String, T> {
+    switch node.content {
+    case .Attrs(let attrs):
+        if let val = attrs[attr] {
+            if let expr = handle(val) {
+                return .right(expr)
+            }
+            else {
+                return .left("Unexpected value for attribute \(attr) at node \(node.id); expected \(expected), found: \(val)")
+            }
+        }
+        else {
+            return .right(defaultValue)
+        }
+    case .Empty:
+        return .right(defaultValue)
+    default:
+        return .left("Missing required attribute \(attr) at node \(node.id) (no attributes present)")
+    }
+}
+
+public func optionalAttrNode<T>(
+    _ node: Node, _ attr: AttrName,
+    expected: String,
+    defaultValue: T,
+    handle: (Node) -> T?
+) -> Either<String, T> {
+    optionalAttr(node, attr, expected: expected, defaultValue: defaultValue) { val in
+        switch val {
+        case .Prim(_):
+            return nil
+        case .Node(let node):
+            return handle(node)
+        }
+    }
+}
