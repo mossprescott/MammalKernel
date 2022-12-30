1ee5bc6243c34cb25515df31338059dbfd7d9fdd
Moss Prescott <moss@theprescotts.com>
2022-12-30 09:28:09 -0500

    A simple, relatively efficient trie-based map data structure

---
+++ Sources/MammalKernel/SimpleTrie.swift
@@ -0,0 +1,175 @@
+import Foundation
+
+/// Constants controlling the branch factor, which is always a power of 2. Smaller values mean allocating more, smaller tables, and
+/// a (slightly) deeper tree. Larger values mean a shallower tree, and fewer, larger table allocations.
+///
+/// 32 is the conventional choice, giving two-level trees of up to 1024 keys, four-level trees up to a million.
+///
+/// For our purposes, trees probably have only tens or hundreds of keys, and they're compact, so we'll have 1-2 levels regardless.
+@usableFromInline
+internal let BITS: UInt = 5
+
+@usableFromInline
+internal let TABLE_SIZE: UInt = 1 << BITS
+
+
+/// Array-mapped Trie data structure, for mapping integer keys to arbitrary values, with efficient, non-destructive updates.
+///
+/// - the keys can be any set of non-negative integers, not necessarily starting at 0
+/// - no "remove" operation is provided; it would be a little bit involved to implement while guaranteeing the result is minimal
+/// - for best performance, use a "compact" set of keys; that is, a sequence of values with few unused keys mixed in
+///
+/// Performance
+/// - the large branch factor means the tree is never more than a few levels deep
+/// - lookups are effectively O(1)
+/// - updates do O(1) allocations
+public struct SimpleTrie<Value> {
+
+    @usableFromInline
+    var node: Node
+
+    public init() {
+        node = SimpleTrie<Value>.empty()
+    }
+
+    /// The number of entries the trie is able to store without increasing its depth (and therefore its memory footprint.) Equivalently,
+    /// one more than the maximum key that can be associated with a value without increasing the memory footprint.
+    ///
+    /// O(1)
+    public func capacity() -> Int {
+        switch node {
+        case .Leaf(_):
+            return Int(TABLE_SIZE)
+        case .NonLeaf(let level, _):
+            return 1 << (BITS*(level+1))
+        }
+    }
+
+    ///
+    @usableFromInline
+    enum Node {
+        case Leaf([Value?])
+        case NonLeaf(level: UInt, [Node])
+    }
+
+    /// A empty table, used to initialize any new node. Ideally would be shared.
+    @usableFromInline
+    static func empty() -> Node { Node.Leaf(Array(repeating: nil, count: Int(TABLE_SIZE))) }
+}
+
+extension SimpleTrie {
+//    @inlinable
+    public subscript(key: UInt) -> Value? {
+        get {
+            guard key < capacity() else {
+                return nil
+            }
+
+            func find(_ idx: UInt, _ node: Node) -> Value? {
+                switch node {
+                case .Leaf(let table):
+                    return table[Int(idx)]
+                case .NonLeaf(let level, let children):
+                    let indexes = indexes(level: level, idx)
+                    return find(indexes.inner, children[Int(indexes.outer)])
+                }
+            }
+
+            return find(UInt(key), node)
+        }
+
+        set {
+            // Extend the structure to the required depth:
+            while key >= capacity() {
+                var currentLevel: UInt
+                switch node {
+                case .Leaf(_):
+                    currentLevel = 0
+                case .NonLeaf(let level, _):
+                    currentLevel = level
+                }
+
+                // Create a chain of nodes all referring to the same, empty Leaf at the bottom:
+                func emptyChain(depth: UInt) -> Node {
+                    if depth > 0 {
+                        let next = emptyChain(depth: depth-1)
+                        return .NonLeaf(level: depth, Array(repeating: next, count: Int(TABLE_SIZE)))
+                    }
+                    else {
+                        return SimpleTrie<Value>.empty()
+                    }
+                }
+                let chain = emptyChain(depth: currentLevel)
+                node = .NonLeaf(level: currentLevel + 1, [node] + Array(repeating: chain, count: Int(TABLE_SIZE)-1))
+            }
+
+            // Construct a new node by replacing a single value in one of the leaves, building a new
+            // node at each intermediate level:
+            func rebuild(_ idx: UInt, _ node: Node) -> Node {
+                switch node {
+                case .Leaf(let table):
+                    var newTable = table
+                    newTable[Int(idx)] = newValue
+                    return .Leaf(newTable)
+
+                case .NonLeaf(let level, let children):
+                    let indexes = indexes(level: level, idx)
+                    var newChildren = children
+                    newChildren[Int(indexes.outer)] = rebuild(indexes.inner, newChildren[Int(indexes.outer)])
+                    return .NonLeaf(level: level, newChildren)
+                }
+            }
+
+            node = rebuild(UInt(key), node)
+        }
+    }
+
+    @inlinable
+    func indexes(level: UInt, _ idx: UInt) -> (outer: UInt, inner: UInt) {
+        let innerBits = level*BITS
+        return (outer: idx >> innerBits,
+                inner: idx & ((1 << innerBits) - 1))
+    }
+}
+
+extension SimpleTrie: Sequence {
+    /// Count the number of (non-nil) values, in the most obvious and brain-dead way.
+    ///
+    /// O(n)
+    ///
+    /// Note: a more efficient implementation is possible, by tracking counts in the nodes, or just by keeping track of empty tables.
+    public var count: Int {
+        func go(_ node: Node) -> Int {
+            switch node {
+            case .Leaf(let table):
+                return table.reduce(0) { (acc, val) in acc + (val != nil ? 1 : 0) }
+            case .NonLeaf(_, let nodes):
+                return nodes.reduce(0) { (acc, child) in acc + go(child) }
+            }
+        }
+        return go(node)
+    }
+
+    public func makeIterator() -> Iterator {
+        return Iterator()
+    }
+
+    public class Iterator: IteratorProtocol {
+        var nextIndex: UInt = 0
+
+        public func next() -> (UInt, Value)? {
+           // if nextIndex < capacity()
+            // TODO
+            fatalError("TODO")
+        }
+    }
+}
+
+// MARK: - Debugging aids
+
+extension SimpleTrie: CustomDebugStringConvertible {
+    public var debugDescription: String {
+        return "trie..."
+        // TODO: summarize the tree structure?
+    }
+}
---
+++ Tests/MammalKernelTests/SimpleTrieTests.swift
@@ -0,0 +1,136 @@
+import XCTest
+import MammalKernel
+
+class SimpleTrieTests: XCTestCase {
+    func testEmpty() throws {
+        let map = SimpleTrie<String>()
+
+        XCTAssertNil(map[0])
+        XCTAssertEqual(map.count, 0)
+        XCTAssertEqual(map.capacity(), 32)
+    }
+
+    func testOne() throws {
+        var map = SimpleTrie<String>()
+        map[0] = "abc"
+
+        XCTAssertEqual(map[0], "abc")
+        XCTAssertEqual(map.count, 1)
+        XCTAssertEqual(map.capacity(), 32)
+    }
+
+    func testBigOne() throws {
+        var map = SimpleTrie<String>()
+        map[100] = "abc"
+
+        XCTAssertEqual(map[100], "abc")
+        XCTAssertEqual(map.count, 1)
+        XCTAssertEqual(map.capacity(), 1024)
+    }
+
+    func testMany() throws {
+        var map = SimpleTrie<Int>()
+
+        for i: UInt in 0..<1000 {
+            map[i] = Int(i)
+        }
+
+        for i: UInt in 0..<1000 {
+            XCTAssertEqual(map[i], Int(i))
+        }
+
+        XCTAssertNil(map[1000])
+
+        XCTAssertEqual(map.count, 1000)
+        XCTAssertEqual(map.capacity(), 1024)
+    }
+
+// MARK: Persistence
+
+    func testPersistent() throws {
+        var map1 = SimpleTrie<String>()
+        map1[0] = "abc"
+        map1[1] = "def"
+
+        var map2 = map1
+        map2[1] = "ghi"
+
+        // map1 is unchanged after a "copy" of it is "modified":
+        XCTAssertEqual(map1[0], "abc")
+        XCTAssertEqual(map1[1], "def")
+
+        XCTAssertEqual(map2[0], "abc")
+        XCTAssertEqual(map2[1], "ghi")
+    }
+
+    func testFarLookup() throws {
+        var map = SimpleTrie<String>()
+        map[0] = "abc"
+
+        let initialCapacity = map.capacity()
+
+        let missing = map[UInt(initialCapacity*10 + 13)]
+
+        XCTAssertNil(missing)
+        XCTAssertEqual(map.count, 1)
+
+        // The capacity is unaffected by a lookup:
+        XCTAssertEqual(map.capacity(), initialCapacity)
+    }
+
+
+// MARK: Performace
+
+    /// Write a large number of values, starting with an empty map.
+    ///
+    /// Best performance: 4-6 bits
+    func testWritePerformance() throws {
+        var map = SimpleTrie<Int>()
+
+        self.measure {
+            for i: UInt in 0..<100_000 {
+                map[i] = Int(i)
+            }
+        }
+
+        print("capacity: \(map.capacity())")
+    }
+
+    /// Write a large number of values, spread out so there's only one or two values per leaf node, starting with an empty map.
+    ///
+    /// Best performance: 5 bits
+    func testSparseWritePerformance() throws {
+        var map = SimpleTrie<Int>()
+
+        self.measure {
+            for i: UInt in 0..<100_000 {
+                map[11*i] = Int(i)
+            }
+        }
+
+        print("capacity: \(map.capacity())")
+    }
+
+    /// Read from successive indexes, repeating the whole sequence several times.
+    ///
+    /// Best performance: pretty flat for 5-9 bits.
+    func testReadPerformance() throws {
+        var map = SimpleTrie<Int>()
+        for i: UInt in 0..<1024 {
+            map[i] = Int(i)
+        }
+
+        var sum: Int = 0
+
+        self.measure {
+            for i: UInt in 0..<200*1024 {
+                sum += map[i % 1024] ?? 0
+            }
+        }
+
+        print("sum: \(sum)")
+        print("capacity: \(map.capacity())")
+    }
+
+
+}
